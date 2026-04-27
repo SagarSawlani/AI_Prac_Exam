372514d728a353c5993098df1d6f422f5fd22030
SagarSawlani <157211856+SagarSawlani@users.noreply.github.com>
2026-04-27 08:27:22 +0530

    Update Exp1_dfs.py

--- Exp1_dfs.py
+++ Exp1_dfs.py
@@ -1,36 +1,3 @@
-# def dfs(graph, start):
-#   visited = set()
-#   stack = [start]
-#   i = 0
-#   print(f"\n\nIteration {i}: Stack: {stack}")
-#   traversal_order = []
-
-#   while stack:
-#     node = stack.pop()
-#     if node not in visited:
-#       visited.add(node)
-#       traversal_order.append(node)
-
-#       for neighbour in reversed(graph[node]):
-#         if neighbour not in visited:
-#           stack.append(neighbour)
-#       i += 1
-#       print(f"\n\nIteration {i}: Stack: {stack}, Visited: {visited}")
-          
-#   return traversal_order
-
-# # Graph Input
-# graph = {}
-# n = int(input("Enter the number of nodes in the graph: "))
-# for i in range(n):
-#   node = input("Enter Node Name: ")
-#   neighbours = input(f"Enter neighbouring nodes of {node} (comma seperated): ").split(',')
-#   graph[node] = neighbours
-
-# start_node = input("Enter the Starting Node for DFS: ")
-# traversal_order = dfs(graph, start_node)
-# print(f"\n\nDFS Traversal Order: {traversal_order}")
-
 def dfs(graph, start, goal):
   visited = set()
   stack = [start]
